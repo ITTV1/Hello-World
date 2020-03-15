e479254948b6cd66abac632aac5d23b7aee03b66
ITTV1 <62187261+ITTV1@users.noreply.github.com>
2020-03-15 21:02:10 +0200

    Add files via upload

---
+++ hosts.php
@@ -0,0 +1,22 @@
+Hosts
+
+UNIX-
+подобной операционной системе можно обнаружить по пути /etc/hosts, а в Windows по
+пути C:\Windows\system32\drivers\etc\hosts
+
+127.0.0.1
+::1
+localhost
+localhost
+
+
+127.0.0.1   site1.dev
+127.0.0.1  www.site1.dev
+127.0.0.2  project1.dev
+127.0.0.2  www.project1.dev
+
+
+
+
+
+
